53a4e29acda53c5ca570f663f827516677369d56
sai shravan <saishravan384@gmail.com>
2024-04-20 21:29:14 +0530

    feat: Create `CategorySchema` and `ItemSchema`

---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 32 },
+  description: { type: String, required: true, minLength: 3, maxLength: 100 },
+  price: { type: Number, min: 1, required: true },
+  number_in_stock: { type: Number, min: 0, required: true },
+  category: { type: [Schema.Types.ObjectId], ref: 'Category', required: true },
+})
+
+ItemSchema.virtual('url').get(function () {
+  return `/inventory/item/${this._id}`
+})
+
+module.exports = mongoose.model('Item', ItemSchema)
