7b6198a87de5b800eb3c0011fc0d803bf8aded07
sai shravan <saishravan384@gmail.com>
2024-04-29 21:39:44 +0530

    feat: Allow item creation and update having images

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,10 @@
 const Item = require('../models/item')
 const Category = require('../models/category')
+const multerUtils = require('../utils/multer.util')
 const asyncHandler = require('express-async-handler')
+const cloudinaryUtils = require('../utils/cloudinary.util')
 const { body, validationResult } = require('express-validator')
+const fs = require('fs')
 
 // Display list of all Items.
 exports.itemList = asyncHandler(async (req, res) => {
@@ -44,6 +47,18 @@ exports.itemCreateGet = asyncHandler(async (req, res) => {
 
 // Handle Item create on POST.
 exports.itemCreatePost = [
+  // upload the images using multer
+  multerUtils.upload.fields([
+    {
+      name: 'titleImg',
+      maxCount: 1,
+    },
+    {
+      name: 'heroImg',
+      maxCount: 1,
+    },
+  ]),
+
   // Convert the categories into an array.
   (req, res, next) => {
     if (!Array.isArray(req.body.category)) {
@@ -76,7 +91,7 @@ exports.itemCreatePost = [
     .withMessage('The minimum price should be 1.')
     .escape(),
 
-  body('number_in_stock')
+  body('numInStock')
     .toInt()
     .isLength({ min: 0 })
     .withMessage('The minimum number of stock should be 0.')
@@ -87,24 +102,64 @@ exports.itemCreatePost = [
     .withMessage('You should select at least one category')
     .escape(),
 
+  body('publisher')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Publisher name must be atleast 3 characters long.')
+    .isLength({ max: 32 })
+    .withMessage('Publisher name must be a maximum of 32 characters long.')
+    .escape(),
+
+  body('rating')
+    .toFloat()
+    .isLength({ min: 0 })
+    .withMessage('The minimum rating should be 0.')
+    .isLength({ max: 5 })
+    .withMessage('The maximum rating should be 5.')
+    .escape(),
+
+  body('releaseDate', 'Invalid date').isISO8601().toDate(),
+
   // Process request after validation and sanitization.
   asyncHandler(async (req, res) => {
     const errors = validationResult(req)
+    const imageErrors = []
 
+    // create Item object with sanitized data
     const item = new Item({
       name: req.body.name,
       description: req.body.description,
       price: req.body.price,
-      number_in_stock: req.body.number_in_stock,
+      numInStock: req.body.numInStock,
       category: req.body.category,
+      publisher: req.body.publisher,
+      releaseDate: req.body.releaseDate,
+      rating: req.body.rating,
+      heroImgUrl: req.body.heroImgUrl || '',
+      titleImgUrl: req.body.titleImgUrl || '',
     })
 
-    if (!errors.isEmpty()) {
+    const handleImage = async (image) => {
+      if (!image.mimetype.startsWith('image/')) {
+        imageErrors.push(
+          new Error(`Please upload an image for the ${image.fieldname}.`)
+        )
+      } else {
+        item[`${image.fieldname}Url`] = await cloudinaryUtils.getUploadedUrl(
+          image.path
+        )
+      }
+      fs.unlink(image.path, (err) => err && console.log(err))
+    }
+    for (const key in req.files) {
+      await handleImage(req.files[key][0])
+    }
+
+    if (!errors.isEmpty() || imageErrors.length) {
+      // mark selected categories as checked
       const categories = await Category.find({}, 'name')
         .sort({ name: 1 })
         .exec()
-
-      // mark selected categories as checked
       categories.forEach(
         (category) =>
           item.category.includes(category._id) && (category.checked = true)
@@ -114,7 +169,7 @@ exports.itemCreatePost = [
         title: 'Create Item',
         item,
         categories,
-        errors: errors.array(),
+        errors: [...errors.array(), ...imageErrors],
       })
     } else {
       await item.save()
@@ -139,7 +194,13 @@ exports.itemDeleteGet = asyncHandler(async (req, res) => {
 
 // Handle Item delete on POST.
 exports.itemDeletePost = asyncHandler(async (req, res) => {
-  await Item.findByIdAndDelete(req.body.itemId)
+  const item = await Item.findById(req.params.id, 'titleImgUrl heroImgUrl')
+
+  await Promise.all([
+    cloudinaryUtils.deleteUploadedFile(item.titleImgUrl),
+    cloudinaryUtils.deleteUploadedFile(item.heroImgUrl),
+    await Item.findByIdAndDelete(req.body.itemId),
+  ])
   res.redirect('/inventory/items')
 })
 
@@ -171,6 +232,18 @@ exports.itemUpdateGet = asyncHandler(async (req, res, next) => {
 
 // Handle Item update on POST.
 exports.itemUpdatePost = [
+  // upload the images using multer
+  multerUtils.upload.fields([
+    {
+      name: 'titleImg',
+      maxCount: 1,
+    },
+    {
+      name: 'heroImg',
+      maxCount: 1,
+    },
+  ]),
+
   // Convert the categories into an array.
   (req, res, next) => {
     if (!Array.isArray(req.body.category)) {
@@ -203,7 +276,7 @@ exports.itemUpdatePost = [
     .withMessage('The minimum price should be 1.')
     .escape(),
 
-  body('number_in_stock')
+  body('numInStock')
     .toInt()
     .isLength({ min: 0 })
     .withMessage('The minimum number of stock should be 0.')
@@ -214,25 +287,67 @@ exports.itemUpdatePost = [
     .withMessage('You should select at least one category')
     .escape(),
 
+  body('publisher')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Publisher name must be atleast 3 characters long.')
+    .isLength({ max: 32 })
+    .withMessage('Publisher name must be a maximum of 32 characters long.')
+    .escape(),
+
+  body('rating')
+    .toFloat()
+    .isLength({ min: 0 })
+    .withMessage('The minimum rating should be 0.')
+    .isLength({ max: 5 })
+    .withMessage('The maximum rating should be 5.')
+    .escape(),
+
+  body('releaseDate', 'Invalid date').isISO8601().toDate(),
+
   // Process request after validation and sanitization.
   asyncHandler(async (req, res) => {
     const errors = validationResult(req)
+    const imageErrors = []
 
     const item = new Item({
       _id: req.params.id,
       name: req.body.name,
       description: req.body.description,
       price: req.body.price,
-      number_in_stock: req.body.number_in_stock,
+      numInStock: req.body.numInStock,
       category: req.body.category,
+      publisher: req.body.publisher,
+      releaseDate: req.body.releaseDate,
+      rating: req.body.rating,
+      heroImgUrl: req.body.heroImgUrl || '',
+      titleImgUrl: req.body.titleImgUrl || '',
     })
 
-    if (!errors.isEmpty()) {
+    const handleImage = async (image) => {
+      if (!image.mimetype.startsWith('image/')) {
+        imageErrors.push(
+          new Error(
+            `Please upload a valid image to update the ${image.fieldname}.`
+          )
+        )
+      } else {
+        const [newImgUrl] = await Promise.all([
+          cloudinaryUtils.getUploadedUrl(image.path),
+          cloudinaryUtils.deleteUploadedFile(item[`${image.fieldname}Url`]),
+        ])
+        item[`${image.fieldname}Url`] = newImgUrl
+      }
+      fs.unlink(image.path, (err) => err && console.log(err))
+    }
+    for (const key in req.files) {
+      await handleImage(req.files[key][0])
+    }
+
+    if (!errors.isEmpty() || imageErrors.length) {
       const categories = await Category.find({}, 'name')
         .sort({ name: 1 })
         .exec()
-
-      // mark selected categories as checked
       categories.forEach(
         (category) =>
           item.category.includes(category._id) && (category.checked = true)
@@ -242,11 +357,10 @@ exports.itemUpdatePost = [
         title: 'Update Item',
         item,
         categories,
-        errors: errors.array(),
+        errors: [...errors.array(), ...imageErrors],
       })
     } else {
       const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {})
-
       res.redirect(updatedItem.url)
     }
   }),
--- models/item.js
+++ models/item.js
@@ -4,14 +4,49 @@ const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
   name: { type: String, required: true, minLength: 3, maxLength: 32 },
+
   description: { type: String, required: true, minLength: 3, maxLength: 500 },
+
   price: { type: Number, min: 1, required: true },
-  number_in_stock: { type: Number, min: 0, required: true },
+
+  numInStock: { type: Number, min: 0, required: true },
+
   category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+
+  titleImgUrl: { type: String },
+
+  heroImgUrl: { type: String },
+
+  publisher: { type: String, minLength: 3, maxLength: 32 },
+
+  releaseDate: { type: Date },
+
+  rating: { type: Number, min: 0, max: 5, default: 1 },
 })
 
 ItemSchema.virtual('url').get(function () {
   return `/inventory/item/${this._id}`
+})
+
+ItemSchema.virtual('releaseDateFormatted').get(function () {
+  return this.releaseDate.toDateString() // format as: Mon 3 April 2000
+})
+
+ItemSchema.virtual('releaseDateForDateInput').get(function () {
+  const month =
+    this.releaseDate.getMonth() + 1 < 10
+      ? `0${this.releaseDate.getMonth() + 1}`
+      : this.releaseDate.getMonth() + 1
+
+  const date =
+    this.releaseDate.getDate() < 10
+      ? `0${this.releaseDate.getDate()}`
+      : this.releaseDate.getDate()
+
+  const dateString = `${this.releaseDate.getFullYear()}-${month}-${date}`
+
+  return dateString
+  // format as: 'YYYY-MM-DD' => 2000-04-03
 })
 
 module.exports = mongoose.model('Item', ItemSchema)
